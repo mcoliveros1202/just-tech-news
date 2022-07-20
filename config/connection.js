31d5f96d785e5a4fa560011763be019994d43bbf
moliveros <mcoliveros1202@gmail.com>
2022-07-20 18:31:25 -0500

    lesson 1

---
+++ config/connection.js
@@ -0,0 +1,15 @@
+// import the Sequelize constructor from the library
+
+const Sequelize = require('sequelize');
+
+require('dotenv').config();
+
+// create connection to our db
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, null, {
+  host: 'localhost',
+  dialect: 'mysql',
+  password: null,
+  port: 3306
+});
+
+module.exports = sequelize;
