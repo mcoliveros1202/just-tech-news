35cab8643cc460d62f33f879a6b8c35cd7e2b17c
moliveros <mcoliveros1202@gmail.com>
2022-07-21 08:06:10 -0500

    hashed password

--- models/User.js
+++ models/User.js
@@ -1,46 +1,41 @@
 const { Model, DataTypes } = require('sequelize');
-const sequelize = require('../config/connection');
 const bcrypt = require('bcrypt');
+const sequelize = require('../config/connection');
+
 
 // create User model
-class User extends Model { }
+class User extends Model { 
+      // set up method to run on instance data (per user) to check password
+  checkPassword(loginPw) {
+    return bcrypt.compareSync(loginPw, this.password);
+  }
+}
 
 // define table columns and configuration
 User.init(
     {
-        // define an id column
         id: {
-            // use the special Sequelize DataTypes object provide what type of data it is
             type: DataTypes.INTEGER,
-            // this is the equivalent of SQL's `NOT NULL` option
             allowNull: false,
-            // instruct that this is the Primary Key
             primaryKey: true,
-            // turn on auto increment
             autoIncrement: true
         },
-        // define a username column
         username: {
             type: DataTypes.STRING,
             allowNull: false
         },
-        // define an email column
         email: {
             type: DataTypes.STRING,
             allowNull: false,
-            // there can't be any duplicate email values in table
             unique: true,
-            // if allowNull is set to false, we can run our data through validators before creating the table data
             validate: {
                 isEmail: true
             }
         },
-        // define a password column
         password: {
             type: DataTypes.STRING,
             allowNull: false,
             validate: {
-                // this means the password must be at least four characters long
                 len: [4]
             }
         }
@@ -49,7 +44,7 @@ User.init(
         hooks: {
             // set up beforeCreate lifecycle "hook" functionality
             async beforeCreate(newUserData) {
-                newUserData.password = await bcrypt.hash(newUserData.password, 10)
+                newUserData.password = await bcrypt.hash(newUserData.password, 10);
                 return newUserData;
             },
             // set up beforeUpdate lifecycle "hook" functionality
@@ -58,15 +53,10 @@ User.init(
                 return updatedUserData;
             }
         },
-        // pass in our imported sequelize connection (the direct connection to our database)
         sequelize,
-        // don't automatically create createdAt/updatedAt timestamp fields
         timestamps: false,
-        // don't pluralize name of database table
         freezeTableName: true,
-        // use underscores instead of came-casing (i.e. `comment_text` and not `commentText`)
         underscored: true,
-        // make it so our model name stays lowercase in the database
         modelName: 'user'
     }
 );
