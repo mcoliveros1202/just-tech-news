fc49a9cc545add6f04500e4235268d4e68622919
moliveros <mcoliveros1202@gmail.com>
2022-07-25 20:37:07 -0500

    added heroku

--- config/connection.js
+++ config/connection.js
@@ -4,11 +4,16 @@ const Sequelize = require('sequelize');
 require('dotenv').config();
 
 // create connection to our db
-const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, null, {
-  host: 'localhost',
-  dialect: 'mysql',
-  password: null,
-  port: 3306
-});
+let sequelize;
 
+if (process.env.JAWSBD_URL) {
+  sequelize = new Sequelize(process.env.JAWSDB_URL);
+} else {
+  sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, null, {
+    host: 'localhost',
+    dialect: 'mysql',
+    password: null,
+    port: 3306
+  });
+}
 module.exports = sequelize;
